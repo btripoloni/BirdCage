dc1cd1438b020053f853aff1305e9b974f5b5d3e
Bruno Tripoloni <bruno.tripoloni@proton.me>
2024-03-29 17:55:56 -0300

    Set the instances.json path to home folder.

--- src/main/ipc/InstancesManager.ts
+++ src/main/ipc/InstancesManager.ts
@@ -1,20 +1,43 @@
 import fs from 'fs'
 import { Instances } from '../../renderer/src/types/Instances'
+import { app } from 'electron'
+import path from 'path'
+
+let instacesPath: string = ''
 
 export function InstacesManager(ipc: Electron.IpcMain): void {
+  instacesPath = path.join(userPath(), 'instances.json')
   SaveInstaces(ipc)
   LoadInstaces(ipc)
 }
 
+function userPath(): string {
+  let userDirectoryName: string = ''
+  const plataformList = {
+    win32: 'appData',
+    linux: 'home'
+  }
+  const plataform = plataformList[process.platform]
+
+  if (!plataform) return ''
+
+  userDirectoryName = app.getPath(plataform)
+  const userDirectoryPath = path.join(userDirectoryName, '.bird-cage')
+  if (!fs.existsSync(userDirectoryPath)) {
+    fs.mkdirSync(userDirectoryPath)
+  }
+  return userDirectoryPath
+}
+
 function SaveInstaces(ipc: Electron.IpcMain): void {
   ipc.on('instaces:save', (_event, instances: Instances[]) => {
-    fs.writeFileSync('./instances.json', JSON.stringify(instances, null, 2))
+    fs.writeFileSync(instacesPath, JSON.stringify(instances, null, 2))
   })
 }
 
 async function LoadInstaces(ipc: Electron.IpcMain): Promise<Instances[]> {
   ipc.handle('instaces:load', () => {
-    if (fs.existsSync('./instances.json')) {
+    if (fs.existsSync(instacesPath)) {
       return JSON.parse(fs.readFileSync('./instances.json', 'utf8'))
     }
   })
